f2c804b177753b85603787d10f72fa95e906eeb0
Pato <infotvorai@gmail.com>
2025-11-24 12:31:10 +0100

    Update merge-face.js

--- routes/merge-face.js
+++ routes/merge-face.js
@@ -103,6 +103,7 @@ router.post(
         Key: fileName,
         Body: buffer,
         ContentType: `image/${outType}`,
+        ACL: 'public-read'
       }).promise();
 
       const url = uploadRes.Location; // verejná URL
